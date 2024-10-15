72c9c9333fd1309efa311f2669dc0abf99e48305
umair shah <hafiz.muhammedumair8767@gmail.com>
2024-10-15 21:05:37 +0500

    swipe install

--- app/_components/Banner.jsx
+++ app/_components/Banner.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
+import 'swiper/css/pagination';
 import Image from "next/image";
 import { Autoplay, Pagination } from "swiper/modules";
 import Button from './Button'; // Import the Button component
